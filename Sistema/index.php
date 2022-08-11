ca84248b9c21b884fee8d3f40abd1d960845bebe
Gartiow-tech <69702822+Gartiow-tech@users.noreply.github.com>
2022-08-11 17:53:40 -0300

    Implementando CSS.

--- Sistema/index.php
+++ Sistema/index.php
@@ -5,23 +5,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Clouth High</title>
+    <link rel="icon" href="../Favicon/favicon-16x16.png">
+    <link rel="stylesheet" href="../Estilo/style.css">
 </head>
 
 <body>
-    <h1> Faça o login </h1>
-    <form action="acesso.php" method="post">
-        <label for="username"> Usuario </label>
-        <input type="text" name="login" id="username" /> <br />
-        <label for="password"> Senha: </label>
-        <input type="password" name="senha" id="password" /><br />
-        <input type="submit" value="acessar" />
+
+    <div id="area">
+        <form id="formulario" action="acesso.php" method="post" autocomplete="off" >
+            <fieldset>
+                <legend> Fazer login </legend>
+                <label for="username" class="username"> Usuario:</label> 
+                <br/>
+
+                <input class="campo_login" type="text" name="login" id="username" /> 
+                <label for="password" class="password"> Senha: </label>
+
+                <input class="campo_senha" type="password" name="senha" id="password" /><br />
+                <input type="submit" value="Acessar" id="botao" class="btn_submit" />
+            </fieldset>
 
 
 
 
-
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
--- Sistema/primeiro.php
+++ Sistema/primeiro.php
@@ -10,6 +10,7 @@ require_once("validarestrito.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="Estilo/style.css">
 </head>
 
 <body>
